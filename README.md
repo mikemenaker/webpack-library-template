c7645f145c418a7a2ce70f7c99cbfa379f987d89
Mike Menaker <mikem@voicebox.com>
2018-04-27 15:51:51 -0700

    Updating to latest webpack, adding development mode, async/await support from babel

--- README.md
+++ README.md
@@ -12,6 +12,11 @@
 * `npm login` - log into your npm account
 * `npm publish` - publish to npm using your account and meta data in package.json
 
+## Development mode
+If you would like the development build to publish the output to a local project you can adjust to:
+
+"dev": "webpack --mode development --output-path ./path-to-project/node_modules/webpack-library-template/dist/ --watch",
+
 Example of consuming a library from outside Node:
 
 https://jsfiddle.net/mikemenaker/84aryz45/
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,27 @@
 /* global __dirname, require, module*/
-
-const webpack = require('webpack');
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-const path = require('path');
-const env = require('yargs').argv.env; // use --env with webpack 2
-
-let libraryName = 'ExampleModule';
-
-let plugins = [], outputFile;
-
-if (env === 'production') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
-}
-
 const config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/dist',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
+    output: {
+        filename: 'ExampleModule.js',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, loader: 'babel-loader' },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: false
+                        }
+                    }
+                ]
             }
-          }
         ]
-      }
-    ]
-  },
-  resolve: {
-    modules: [path.resolve('./node_modules'), path.resolve('./src')],
-    extensions: ['.json', '.js']
-  },
-  plugins: plugins
+    }
 };
 
 module.exports = config;
