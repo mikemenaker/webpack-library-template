50cfa8746ca8e3a9aa4fb3e0d8ddda31a5105b38
Mike Menaker <mikem@voicebox.com>
2018-05-11 10:25:25 -0700

    Updates

--- test/library.spec.js
+++ test/library.spec.js
@@ -1,16 +1,15 @@
 /* global describe, it, before */
 
 import chai from 'chai';
-import * as ExampleModule from '../dist/ExampleModule.js';
+import { createParagraphElement } from '../dist/ExampleModule.js';
 
 chai.expect();
 
 const expect = chai.expect;
 
-
 describe('Given an ExampleModule', () => {
-  it('createParagraphElement should be a function', () => {
-    expect(ExampleModule.createParagraphElement).to.be.an('function');
-  });
+    it('createParagraphElement should be a function', () => {
+        expect(createParagraphElement).to.be.an('function');
+    });
 });
 
